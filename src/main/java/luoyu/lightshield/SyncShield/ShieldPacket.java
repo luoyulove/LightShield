c4c7a1725f6240fde12766724c1870238e0ae6ea
luoyu <10410411115@qq.com>
2024-03-03 00:22:55 +0800

    first commit

--- src/main/java/luoyu/lightshield/SyncShield/ShieldPacket.java
+++ src/main/java/luoyu/lightshield/SyncShield/ShieldPacket.java
@@ -1,37 +1,48 @@
-//package luoyu.lightshield.SyncShield;
-//
-//import commonnetwork.Constants;
-//import luoyu.lightshield.PlayerShield;
-//import net.minecraft.client.Minecraft;
-//import net.minecraft.network.FriendlyByteBuf;
-//import commonnetwork.networking.data.PacketContext;
-//import commonnetwork.networking.data.Side;
-//import net.minecraft.resources.ResourceLocation;
-//import net.minecraft.world.entity.player.Player;
-//
-//import java.util.UUID;
-//
-//public class ShieldPacket {
-//    public static final ResourceLocation CHANNEL = new ResourceLocation(Constants.MOD_ID, "shield_channel");
-//    private float shieldAmount;
-//    private UUID player_uuid;
-//
-//    public ShieldPacket(float shieldAmount, UUID player_uuid){
-//        this.shieldAmount = shieldAmount;
-//        this.player_uuid = player_uuid;
-//    }
-//    public static ShieldPacket fromBytes(FriendlyByteBuf buf){
-//        return new ShieldPacket(buf.readFloat(), buf.readUUID());
-//    }
-//    public static void shieldAmountencode(ShieldPacket packet, FriendlyByteBuf buf){
-//        buf.writeFloat(packet.shieldAmount);
-//        buf.writeUUID(packet.player_uuid);
-//    }
-//    public void handle(PacketContext<ShieldPacket> shieldPacket){
-//        if(shieldPacket.side() == Side.SERVER){
-//            float shieldAmount = shieldPacket.message().shieldAmount;
-//            Player player = Minecraft.getInstance().player;
-//            PlayerShield.getPlayerShield(player).setShieldAmount(shieldAmount);
-//        }
-//    }
-//}
+package luoyu.lightshield.SyncShield;
+
+import io.netty.channel.pool.SimpleChannelPool;
+import luoyu.lightshield.LightShield;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.PacketListener;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlerEvent;
+import net.neoforged.neoforge.network.handling.IPacketHandler;
+import net.neoforged.neoforge.network.registration.IPayloadRegistrar;
+
+import java.util.UUID;
+
+public class ShieldPacket implements CustomPacketPayload {
+    private float shieldAmount;
+    private UUID player_uuid;
+
+    public ShieldPacket(float shieldAmount, UUID player_uuid) {
+        this.shieldAmount = shieldAmount;
+        this.player_uuid = player_uuid;
+    }
+
+    public static ShieldPacket fromBytes(FriendlyByteBuf buf) {
+        return new ShieldPacket(buf.readFloat(), buf.readUUID());
+    }
+
+    @Override
+    public void write(FriendlyByteBuf buf) {
+        buf.writeFloat(this.shieldAmount);
+        buf.writeUUID(this.player_uuid);
+    }
+
+    @Override
+    public ResourceLocation id() {
+        return new ResourceLocation(LightShield.MOD_ID, "shield_amount");
+    }
+
+    public float getShieldAmount() {
+        return shieldAmount;
+    }
+
+    public UUID getPlayerUUID() {
+        return player_uuid;
+    }
+}
