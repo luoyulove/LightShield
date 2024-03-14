f2586f01357ef6c492cab1abfb05414c4410e1ad
落羽 <1041041115@qq.com>
2024-03-14 10:48:19 +0800

    version 1.0.5.3

--- src/main/java/luoyu/lightshield/Api.java
+++ src/main/java/luoyu/lightshield/Api.java
@@ -1,15 +1,5 @@
 package luoyu.lightshield;
 
-import luoyu.lightshield.NetWork.SyncShieldSystem;
-import luoyu.lightshield.ShieldSystem.Shield;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.event.TickEvent;
-import net.neoforged.neoforge.network.PacketDistributor;
-
-import static com.mojang.text2speech.Narrator.LOGGER;
-
 public class Api {
     public static float ClientShieldAmount;
     public static void getShieldAmount(float shieldAmount){
