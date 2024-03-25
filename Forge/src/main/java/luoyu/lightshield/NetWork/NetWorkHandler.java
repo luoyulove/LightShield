a0fc88a7190cf12154ff2330c54cbe2a8684635c
落羽 <1041041115@qq.com>
2024-03-25 20:24:22 +0800

    version 1.0.7.1

--- Forge/src/main/java/luoyu/lightshield/NetWork/NetWorkHandler.java
+++ Forge/src/main/java/luoyu/lightshield/NetWork/NetWorkHandler.java
@@ -1,22 +1,16 @@
 package luoyu.lightshield.NetWork;
 
 import luoyu.lightshield.ShieldSystem.Shield;
-import net.minecraft.client.Minecraft;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.network.NetworkDirection;
-import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.simple.SimpleChannel;
 
-import java.util.function.Supplier;
-
 import static luoyu.ModStatic.MOD_ID;
-import static net.minecraft.commands.arguments.EntityArgument.getPlayer;
 
 public class NetWorkHandler {
     public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
