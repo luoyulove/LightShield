c6d4e6b45537488da83c44dfa94b3f8973a91068
luoyu <10410411115@qq.com>
2025-05-04 00:29:35 +0800

    update: BUG修复

--- src/main/java/luoyu/lightshield/Effects/ShieldRegenEffect.java
+++ src/main/java/luoyu/lightshield/Effects/ShieldRegenEffect.java
@@ -8,7 +8,7 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 public class ShieldRegenEffect extends MobEffect {
     public ShieldRegenEffect(){
         super(MobEffectCategory.BENEFICIAL, 0x0000FF);
-        this.addAttributeModifier(ShieldAttribute.SHIELD_MAX.get(), "f6a1b2c3-e123-4d56-7890-a1b2c3d4e5f6",
+        this.addAttributeModifier(ShieldAttribute.SHIELD_REGEN.get(), "f6a1b2c3-e123-4d56-7890-a1b2c3d4e5f6",
                 0.1D, AttributeModifier.Operation.MULTIPLY_BASE);
     }
 }
