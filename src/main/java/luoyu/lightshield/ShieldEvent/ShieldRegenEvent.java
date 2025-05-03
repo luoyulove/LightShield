c7b57a8cd77b0968da3eff4ffaeb57cc2a0b3ae7
luoyu <10410411115@qq.com>
2025-05-03 18:20:11 +0800

    commit

--- src/main/java/luoyu/lightshield/ShieldEvent/ShieldRegenEvent.java
+++ src/main/java/luoyu/lightshield/ShieldEvent/ShieldRegenEvent.java
@@ -1,13 +1,14 @@
 package luoyu.lightshield.ShieldEvent;
 
 import luoyu.lightshield.ShieldSystem.ShieldMain;
-import net.minecraft.server.TickTask;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerEvent;
-import net.neoforged.neoforge.event.tick.EntityTickEvent;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
+import static luoyu.lightshield.LightShield.MODID;
+
+@Mod(MODID)
 public class ShieldRegenEvent {
     @SubscribeEvent
     public void onPlayerTick(PlayerTickEvent.Post event){
